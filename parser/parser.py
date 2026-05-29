457511d3eec42bf8436f3bba2cf1972f4d0137b9
bakyt92 <bakyt92@mail.ru>
2026-05-29 15:09:10 +0400

    Add

--- parser/parser.py
+++ parser/parser.py
@@ -6,6 +6,12 @@ class Parser:
 		data = []
 
 	def read_csv(self, link_file):
+		array = []
 		try:
 			with open(link_file, "r") as file:
-				
+				for line in file:
+					line = line.strip()
+					tmp = line.split()
+					array.append(line)
+		except Exception as e:
+			print (f"Error in reading file: {e}")
