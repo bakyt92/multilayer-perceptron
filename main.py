c3850fcf571028ba09b4761b91aac7167ca384fa
bakyt92 <bakyt92@mail.ru>
2026-05-29 16:38:21 +0400

    update argparse

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import sys
+import argparse
+from parser.parser import Parser
 
 def main():
-	()
+	pars = argparse.ArgumentParser(description='Multilayer perceptron')
+	pars.add_argument("filename", help="Enter name of file")
+	args = pars.parse_args()
+	try:
+		if args.filename:
+			Reader = Parser()
+			Reader.read_csv(args.filename)
+		else: 
+			raise FileNotFoundError("File does not exist")
+	except Exception as e:
+		print(f"Exception in main: {e}")
+		sys.exit(1)
 
 if __name__ == '__main__':
 	main()
