51982fcc805ef161d92f633757e9d42f137b7df5
bakyt92 <bakyt92@mail.ru>
2026-05-31 12:47:17 +0400

    update

--- train/mlp.py
+++ train/mlp.py
@@ -1,4 +1,5 @@
 import sys
+import random
 
 class MLP_Class:
     def __init__(self):
@@ -8,5 +9,20 @@ class MLP_Class:
         self.learning_rate = 0
         self.epochs = 0
     
-    def input(self, X_train_norm, training_labels, l_sizes, epochs=100, learning_rate=0.01):
-        
+    def create_layer(n_in, n_out):
+        W = []
+        for row in range(n_out):
+            row = []
+            for elem in range(n_in):
+                row.append(random.random - 0.5)
+            W.append(row)
+        print(f"New Layer is created")
+        print(f"rows: {len(W)}, columns: {len(W[0])}")
+        return W
+
+    def input(self, X_train_norm, training_labels, l_sizes, epochs, learning_rate):
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        output_size = 2
+        input_size = len(X_train_norm[0])
+        self.layer_sizes = [input_size] + l_sizes + output_size
