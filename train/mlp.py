50cbbe55fdbaf9a5832c208e0d1b66a924f612c1
bakyt92 <bakyt92@mail.ru>
2026-05-31 11:58:09 +0400

    update

--- train/mlp.py
+++ train/mlp.py
@@ -2,17 +2,14 @@ import sys
 
 class MLP:
     def __init__(self):
-        self.w1 = []
-        self.b1 = []
-        self.w2 = []
-        self.b2 = []
-        self.w3 = []
-        self.b3 = []
-        self.w4 = []
-        self.b4 = []
-        size_l1 = 0
-        size_l2 = 0
-        size_l3 = 0
-        epochs = 0
+        self.weights = []
+        self.biases = []
+        self.layer_sizes = []
+        self.learning_rate = 0
+        self.epochs = 0
+    
+    def input(self, ):
+
+        
 
     
