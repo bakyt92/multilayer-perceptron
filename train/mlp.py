a4f93a5cf41ed08b00664bff5ec419f83a2ecef9
bakyt92 <bakyt92@mail.ru>
2026-05-31 13:45:35 +0400

    update

--- train/mlp.py
+++ train/mlp.py
@@ -9,14 +9,14 @@ class MLP_Class:
         self.learning_rate = 0
         self.epochs = 0
     
-    def create_layer(n_in, n_out):
+    def create_layer(self, n_in, n_out):
         if n_in < 1 or n_out < 1:
             return None
         W = []
         for row in range(n_out):
             row = []
             for elem in range(n_in):
-                row.append(random.random - 0.5)
+                row.append(random.random() - 0.5)
             W.append(row)
         print(f"New Layer is created")
         print(f"rows: {len(W)}, columns: {len(W[0])}")
@@ -27,8 +27,13 @@ class MLP_Class:
         self.epochs = epochs
         output_size = 2
         input_size = len(X_train_norm[0])
-        self.layer_sizes = [input_size] + l_sizes + output_size
+        self.layer_sizes = [input_size] + l_sizes + [output_size]
         for size, index in enumerate(self.layer_sizes[1:], start=1):
             self.weights.append(self.create_layer(self.layer_sizes[index - 1], size))
-            self.biases.append(size * 0)
+            self.biases.append(size * [0])
 
+    def ft_activation(self):
+
+
+    def ft_calculation(self):
+        
