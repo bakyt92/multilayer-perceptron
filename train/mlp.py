ce860e12e3ddb4e5e4f21e64e9c303c11132fda9
bakyt92 <bakyt92@mail.ru>
2026-05-31 11:51:10 +0400

    mlp

---
+++ train/mlp.py
@@ -0,0 +1,18 @@
+import sys
+
+class MLP:
+    def __init__(self):
+        self.w1 = []
+        self.b1 = []
+        self.w2 = []
+        self.b2 = []
+        self.w3 = []
+        self.b3 = []
+        self.w4 = []
+        self.b4 = []
+        size_l1 = 0
+        size_l2 = 0
+        size_l3 = 0
+        epochs = 0
+
+    
